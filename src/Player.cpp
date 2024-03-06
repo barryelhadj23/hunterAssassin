1ad73740f4c8ed1f937260cd4dac7e6d5cf45ec4
BAH Mamadou <bah311802@gmail.com>
2024-03-06 02:36:38 +0100

    Separation du noyau et l'IHM, Amelioration du CMakeListe.txt , ajout de Game class et Player class ainsi qu'une structure Rect (x, y, w, h) et aussi un fichier de configuration des variable

---
+++ src/Player.cpp
@@ -0,0 +1,37 @@
+
+#include "Player.h"
+#include "config_Variable.h"
+
+Player::Player() :health(100){
+    source = {0,0,PLAYER_WIDTH,PLAYER_HEIGHT};
+    dest = {100,100,PLAYER_WIDTH*2,PLAYER_HEIGHT*2};
+    spriteIMG = "../data/bardo.bmp";
+}
+
+Player::~Player() {
+ health=0;
+ source={0,0,0,0};
+ dest={0,0,0,0};
+ spriteIMG="";
+}
+
+void Player::testRegression() {
+    //Constructeur par defaut du player
+    Player playerTest;
+    //Test des données du rectangle source
+    assert(playerTest.source.x==0 && playerTest.source.y==0 && playerTest.source.w==PLAYER_WIDTH && playerTest.source.w==PLAYER_WIDTH);
+
+    //Test des données du rectangle dest
+    assert(playerTest.dest.x==100 && playerTest.dest.y==100 && playerTest.dest.w==PLAYER_WIDTH*2 && playerTest.dest.w==PLAYER_WIDTH*2);
+
+    //chemin Image_Bmp
+    assert(playerTest.spriteIMG=="../data/bardo.bmp");
+
+    //Santé du joueur
+    assert(playerTest.health == 100);
+
+    std::cout<<"Teste de regression Nickel ;)";
+}
+
+
+
---
+++ src/mainProject.cpp
@@ -0,0 +1,7 @@
+#include "Game.h"
+
+int main(int argc, char* args[]) {
+    Game g;
+    g.runProject();
+    return 0;
+}
